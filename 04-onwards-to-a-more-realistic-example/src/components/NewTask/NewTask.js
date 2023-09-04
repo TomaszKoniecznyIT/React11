17e5bd82a8dad18b5359212a9d5063bb5de5f0e2
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-09-04 18:36:40 +0100

    custom http hook 4

--- 04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
+++ 04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
@@ -2,40 +2,30 @@ import { useState } from "react";
 
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTasksRequest } = useHttp();
+
+  const createdTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-b0e16-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    sendTasksRequest(
+      {
+        url: "https://react-http-b0e16-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createdTask.bind(null, taskText)
+    );
   };
 
   return (
